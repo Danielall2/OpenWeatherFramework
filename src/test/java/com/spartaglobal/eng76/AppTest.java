24da26b35ddcff2b9abe3d729306ffb1ababdb05
Andrei Pavel <samurahh@gmail.com>
2021-01-26 14:41:26 +0000

    First commit

---
+++ src/test/java/com/spartaglobal/eng76/AppTest.java
@@ -0,0 +1,16 @@
+package com.spartaglobal.eng76;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AppTest 
+{
+
+    @Test
+    @DisplayName("testing")
+    void testing(){
+        Assertions.fail();
+    }
+}
