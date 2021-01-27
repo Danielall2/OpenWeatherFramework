83845c9e06e17843d3226767530f36f6ce6aec67
Andrei Pavel <samurahh@gmail.com>
2021-01-27 11:27:53 +0000

    Added URLBuilder.java v1.0

--- src/test/java/com/spartaglobal/eng76/AppTest.java
+++ src/test/java/com/spartaglobal/eng76/AppTest.java
@@ -1,6 +1,7 @@
 package com.spartaglobal.eng76;
 
 
+import com.spartaglobal.eng76.framework.URLBuilder;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,9 @@ public class AppTest
     @Test
     @DisplayName("testing")
     void testing(){
-        Assertions.fail();
+        System.out.println(URLBuilder.ofCity("Bucharest", "fc40ec94b702609da7483ea4672bb0e6"));
+        System.out.println(URLBuilder.ofCities(new int[]{2643743,683506}, "fc40ec94b702609da7483ea4672bb0e6"));
+        System.out.println(URLBuilder.ofCity(683506, "fc40ec94b702609da7483ea4672bb0e6"));
+        System.out.println(URLBuilder.ofCitiesInRectangle(12,32,15,37,10, "fc40ec94b702609da7483ea4672bb0e6"));
     }
 }
