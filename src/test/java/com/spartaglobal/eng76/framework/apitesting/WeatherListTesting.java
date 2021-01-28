6dca7b41890fb145c432cc37497af3ebf4049b57
NJohnston94 <nathanj.94@hotmail.com>
2021-01-28 11:41:03 +0000

    WeatherListTesting finished to good standard, could be expanded by using different URLBuilder methods

--- src/test/java/com/spartaglobal/eng76/framework/apitesting/WeatherListTesting.java
+++ src/test/java/com/spartaglobal/eng76/framework/apitesting/WeatherListTesting.java
@@ -1,6 +1,5 @@
 package com.spartaglobal.eng76.framework.apitesting;
 
-import com.spartaglobal.eng76.framework.dto.WeatherDTO;
 import com.spartaglobal.eng76.framework.weatherapi.WeatherAPI;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
@@ -13,30 +12,57 @@ import org.junit.jupiter.params.provider.ValueSource;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Properties;
 
 public class WeatherListTesting {
-    static HashMap<String, String> weatherConditions = new HashMap<>(55);
+    static HashMap<String, String>  weatherConditions   = new HashMap<>(55);
+    static Properties               properties          = new Properties();
 
-    static Properties properties = new Properties();
-
+    /**
+     * With given weatherID, get the string from HashMap and split it by ',' so Main,
+     * Description and Icon ID can be retrieved individually from String array
+     * @param weatherID ID number for specific weather condition
+     * @return Main, Description and Icon ID in String array
+     */
     public String[] getWeatherConditions(String weatherID) {
         return weatherConditions.get(weatherID).split(",");
     }
 
+    /**
+     * From getWeatherConditions, take the first item in the array, which is
+     * always Main weather category
+     * @param weatherID ID number for specific weather condition
+     * @return Main weather category
+     */
     public String getWeatherMain(String weatherID) {
         return getWeatherConditions(weatherID)[0].trim();
     }
 
+    /**
+     * From getWeatherConditions, take the second item in the array, which is
+     * always weather Description
+     * @param weatherID ID number for specific weather condition
+     * @return Description of weather
+     */
     public String getWeatherDescription(String weatherID) {
         return getWeatherConditions(weatherID)[1].trim();
     }
 
+    /**
+     * From getWeatherConditions, take the third item in the array, which is
+     * always weather Icon ID
+     * @param weatherID ID number for specific weather condition
+     * @return Icon ID of weather
+     */
     public String getWeatherIconID(String weatherID) {
         return getWeatherConditions(weatherID)[2].trim();
     }
 
+    /**
+     * Add all weather conditions from https://openweathermap.org/weather-conditions to a HashMap.
+     * The HashMap is a local version of the tabled data that can be asserted against by the response(s)
+     * from the live API
+     */
     private static void addWeatherConditions() {
         addWeatherConditionsThunderstorm();
         addWeatherConditionsDrizzle();
@@ -46,6 +72,10 @@ public class WeatherListTesting {
         addWeatherConditionsClouds();
     }
 
+    /**
+     * Add all Thunderstorm weather conditions from https://openweathermap.org/weather-conditions
+     * to a HashMap
+     */
     private static void addWeatherConditionsThunderstorm() {
         weatherConditions.put("200", "Thunderstorm, thunderstorm with light rain, 11d");
         weatherConditions.put("201", "Thunderstorm, thunderstorm with rain, 11d");
@@ -59,6 +89,10 @@ public class WeatherListTesting {
         weatherConditions.put("232", "Thunderstorm, thunderstorm with heavy drizzle, 11d");
     }
 
+    /**
+     * Add all Drizzle weather conditions from https://openweathermap.org/weather-conditions
+     * to a HashMap
+     */
     private static void addWeatherConditionsDrizzle() {
         weatherConditions.put("300", "Drizzle, light intensity drizzle, 09d");
         weatherConditions.put("301", "Drizzle, drizzle, 09d");
@@ -71,6 +105,10 @@ public class WeatherListTesting {
         weatherConditions.put("321", "Drizzle, shower drizzle, 09d");
     }
 
+    /**
+     * Add all Rain weather conditions from https://openweathermap.org/weather-conditions
+     * to a HashMap
+     */
     private static void addWeatherConditionsRain() {
         weatherConditions.put("500", "Rain, light rain, 10d");
         weatherConditions.put("501", "Rain, moderate rain, 10d");
@@ -84,6 +122,10 @@ public class WeatherListTesting {
         weatherConditions.put("531", "Rain, ragged shower rain, 10d");
     }
 
+    /**
+     * Add all Snow weather conditions from https://openweathermap.org/weather-conditions
+     * to a HashMap
+     */
     private static void addWeatherConditionsSnow() {
         weatherConditions.put("600", "Snow, light snow, 13d");
         weatherConditions.put("601", "Snow, Snow, 13d");
@@ -98,6 +140,10 @@ public class WeatherListTesting {
         weatherConditions.put("622", "Snow, Heavy shower snow, 13d");
     }
 
+    /**
+     * Add all Atmosphere weather conditions from https://openweathermap.org/weather-conditions
+     * to a HashMap
+     */
     private static void addWeatherConditionsAtmosphere() {
         weatherConditions.put("701", "Mist, mist, 50d");
         weatherConditions.put("711", "Smoke, smoke, 50d");
@@ -111,12 +157,16 @@ public class WeatherListTesting {
         weatherConditions.put("781", "Tornado, tornado, 50d");
     }
 
+    /**
+     * Add all Clear/Clouds weather conditions from https://openweathermap.org/weather-conditions
+     * to a HashMap
+     */
     private static void addWeatherConditionsClouds() {
         weatherConditions.put("800", "Clear, clear sky, 01d 01n");
-        weatherConditions.put("801", "Clouds, few clouds: 11-25%, 02d 02n");
-        weatherConditions.put("802", "Clouds, scattered clouds: 25-50%, 03d 03n");
-        weatherConditions.put("803", "Clouds, broken clouds: 51-84%, 04d 04n");
-        weatherConditions.put("804", "Clouds, overcast clouds: 85-100%, 04d 04n");
+        weatherConditions.put("801", "Clouds, few clouds, 02d 02n");
+        weatherConditions.put("802", "Clouds, scattered clouds, 03d 03n");
+        weatherConditions.put("803", "Clouds, broken clouds, 04d 04n");
+        weatherConditions.put("804", "Clouds, overcast clouds, 04d 04n");
     }
 
     @BeforeAll
@@ -164,7 +214,10 @@ public class WeatherListTesting {
 
     @ParameterizedTest
     @CsvSource({"leeds" ,
-                "berlin"})
+                "berlin",
+                "lasvegas",
+                "johannesburg",
+                "brasilia"})
     @DisplayName("Does the current weather condition(s) have a valid ID?")
     void isResponseWeatherIDValid(String city)  {
         for(HashMap<String, String> currentWeather:WeatherAPI.ofCity(city, properties.getProperty("apikey")).getWeather()) {
@@ -175,7 +228,10 @@ public class WeatherListTesting {
 
     @ParameterizedTest
     @CsvSource({"leeds" ,
-                "berlin"})
+                "berlin",
+                "lasvegas",
+                "johannesburg",
+                "brasilia"})
     @DisplayName("Does the current weather condition(s) have a valid Main category?")
     void isResponseWeatherMainValid(String city) {
         for(HashMap<String, String> currentWeather:WeatherAPI.ofCity(city, properties.getProperty("apikey")).getWeather()) {
@@ -186,8 +242,11 @@ public class WeatherListTesting {
     }
 
     @ParameterizedTest
-    @CsvSource({"leeds",
-                "berlin"})
+    @CsvSource({"leeds" ,
+                "berlin",
+                "lasvegas",
+                "johannesburg",
+                "brasilia"})
     @DisplayName("Does the current weather condition(s) have a valid Description?")
     void isResponseWeatherDescriptionValid(String city) {
         for(HashMap<String, String> currentWeather:WeatherAPI.ofCity(city, properties.getProperty("apikey")).getWeather()) {
@@ -198,9 +257,11 @@ public class WeatherListTesting {
     }
 
     @ParameterizedTest
-    @CsvSource({"leeds",
+    @CsvSource({"leeds" ,
                 "berlin",
-                "london"})
+                "lasvegas",
+                "johannesburg",
+                "brasilia"})
     @DisplayName("Does the current weather condition(s) have a valid Icon ID?")
     void isResponseWeatherIconValid(String city) {
         for(HashMap<String, String> currentWeather:WeatherAPI.ofCity(city, properties.getProperty("apikey")).getWeather()) {
@@ -215,4 +276,5 @@ public class WeatherListTesting {
             }
         }
     }
+
 }
